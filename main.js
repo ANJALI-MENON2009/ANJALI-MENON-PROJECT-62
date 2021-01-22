9ecb463d259f50c5fa481354276814865d7ad05d
ANJALI-MENON2009 <77269882+ANJALI-MENON2009@users.noreply.github.com>
2021-01-22 09:13:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function alert1() {
+    window.alert(
+        " This is my father. I love him a lot! ❣ He is the BEST papa in the world!"
+    );
+}
+
+function alert2() {
+    window.alert(
+        "This is my mother. I love her a lot too!💗 She is the GREATEST mom one can ever wish for!"
+    );
+}
+
+function alert3() {
+    window.alert(
+        "This is my dear elder brother. He is very dear to me!💖 He is the finest brother ever born!"
+    );
+}
+
+function alert4() {
+    window.alert(
+        "And this of course is me!😃"
+    );
+}
